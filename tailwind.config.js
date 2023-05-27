30e3d2b6a11d11df1a9f0cabe513b47b06df7783
talhatahirfstg <talha.tahir@filestage.io>
2023-05-28 01:27:46 +0500

    fix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   experimental: {
@@ -28,7 +28,7 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.teal,
+        primary: colors.green,
         gray: colors.neutral,
       },
       typography: (theme) => ({
@@ -165,4 +165,4 @@ module.exports = {
     },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-}
+};
