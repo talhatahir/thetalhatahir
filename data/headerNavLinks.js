04ebf5b2915a57182c8fd3aa066fcdc3e1b46b0f
talhatahirfstg <talha.tahir@filestage.io>
2023-05-27 18:53:56 +0500

    fix prettier | remove sparrowhawk

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  { href: '/resume', title: 'Resume' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-]
+];
 
-export default headerNavLinks
+export default headerNavLinks;
