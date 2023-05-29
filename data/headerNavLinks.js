7f92951626c6792497a27e7e833ee7d38949cacf
talhatahirfstg <talha.tahir@filestage.io>
2023-05-29 20:44:08 +0500

    add tags

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
 ];
 
