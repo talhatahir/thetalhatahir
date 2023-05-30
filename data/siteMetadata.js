340a417ead3b46b4d90cd8e7d7fc5878447e57bc
talhatahirfstg <talha.tahir@filestage.io>
2023-05-30 12:34:12 +0500

    add analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -21,7 +21,7 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-TVD4T8Q6FC',
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
