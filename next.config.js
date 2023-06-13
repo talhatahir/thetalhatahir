a4c0b2bebfd4681c72301a442a88cea36c3d9e36
talhatahir <trinitro619@gmail.com>
2023-06-13 13:59:14 +0500

    fix disqus enmbed issue

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
+  default-src 'self' https://disqus.com https://c.disquscdn.com;
   script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com https://www-thetalhatahir-com.disqus.com;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src https://disqus.com;
 `;
 
 const securityHeaders = [
