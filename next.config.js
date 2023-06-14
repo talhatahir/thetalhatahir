43b82a5b86e128af509b9566d201e8584e26d522
talhatahir <trinitro619@gmail.com>
2023-06-15 01:35:11 +0500

    add frm src

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const ContentSecurityPolicy = `
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src https://disqus.com;
+  frame-src https://www-thetalhatahir-com.disqus.com;
   fetch-src 'self' 'no-cors';
 `;
 
