eca863217a19cb8d23090c2564459304cebfec9d
talhatahir <trinitro619@gmail.com>
2023-06-15 13:51:22 +0500

    another fix

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self' https://disqus.com https://c.disquscdn.com;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com https://www-thetalhatahir-com.disqus.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com https://www-thetalhatahir-com.disqus.com/embed.js;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src 'self' 'unsafe-eval' 'unsafe-inline' https://disqus.com https://c.disquscdn.com https://www-thetalhatahir-com.disqus.com;
+  frame-src 'self' 'unsafe-eval' 'unsafe-inline' https://disqus.com https://c.disquscdn.com https://www-thetalhatahir-com.disqus.com/embed.js;
 `;
 
 const securityHeaders = [
