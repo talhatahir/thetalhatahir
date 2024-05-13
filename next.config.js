980d856dfba16426d24bca51ded6a0babb99da0b
Talha Tahir <trinitro619@gmail.com>
2024-05-13 12:32:47 +0500

    update config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-// You might need to insert additional domains in script-src if you are using external services
+// You might need to insert additional domains in script-src if you are using external services.
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com;
