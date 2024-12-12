2ba2ae963550bf518250287748a73b39f6e8b770
Talha Tahir <trinitro619@gmail.com>
2024-12-12 12:48:11 +0500

    add async

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,6 +42,7 @@ const Header = () => {
         <MobileNav />
       </div>
       <script
+        async
         data-name="BMC-Widget"
         data-cfasync="false"
         src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
