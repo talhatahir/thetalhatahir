5d0a4e832ed2c0e99a36c92e98a890669cbda303
Talha Tahir <trinitro619@gmail.com>
2024-12-23 12:58:08 +0500

    add random color to github graph

--- app/api/contributions/route.ts
+++ app/api/contributions/route.ts
@@ -1,6 +1,35 @@
+export const themes = [
+  {
+    name: 'GitHub',
+    color: '2da44e',
+  },
+  {
+    name: 'Ruby Red',
+    color: 'e74c3c',
+  },
+  {
+    name: 'Golden Sun',
+    color: 'f1c40f',
+  },
+  {
+    name: 'Electric Indigo',
+    color: '6610f2',
+  },
+  {
+    name: 'Midnight Blue',
+    color: '2c3e50',
+  },
+  {
+    name: 'Magenta',
+    color: 'D81B60',
+  },
+]
+
 const handler = async () => {
   try {
-    const res = await fetch('https://www.gitch.art/api/og/talhatahir?color=f1c40f', {
+    const randomTheme = themes[Math.floor(Math.random() * themes.length)]
+
+    const res = await fetch(`https://www.gitch.art/api/og/talhatahir?color=${randomTheme.color}`, {
       headers: {
         'Content-Type': 'image/svg+xml',
       },
